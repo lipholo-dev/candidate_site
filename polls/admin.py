19c49c82ecd9dd04341537167a372aa85628e7ef
Dipholo <lipholomoeketsi2@gmail.com>
2025-04-07 16:04:42 +0200

    Add docstrings to admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-# Register the Question and Choice models
+"""
+ Registering the Question and Choice models
+ """
 admin.site.register(Question)
 admin.site.register(Choice)
